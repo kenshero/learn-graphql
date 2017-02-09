aa2dee1290d3f73d09503ffc876133c396ce1f72
kenshero <teeruk@proteus-tech.com>
2017-02-10 03:51:33 +0700

    init file

---
+++ part1/data.js
@@ -0,0 +1,56 @@
+var products = [{
+    name: "Ninetendo Switch",
+    price: "11000",
+    category: ["Computer Game","Game"],
+    vote:[{ 
+      one_star: 20,
+      men: 10,
+      women: 10 
+    },{ 
+      two_star: 60,
+      men: 30,
+      women: 30 
+    },{ 
+      three_star: 120,
+      men: 80,
+      women: 40 
+    }]
+  },
+  {
+    name: "Rebel 300",
+    price: "145000",
+    category: ["Motorcycle"],
+    vote:[{ 
+      one_star: 50,
+      men: 40,
+      women: 10 
+    },{ 
+      two_star: 60,
+      men: 30,
+      women: 30 
+    },{ 
+      three_star: 140,
+      men: 120,
+      women: 20 
+    }]
+  },
+  {
+    name: "Macbook pro",
+    price: "74900",
+    category: ["Electronic","Laptop"],
+    vote:[{ 
+      one_star: 5,
+      men: 4,
+      women: 1 
+    },{ 
+      two_star: 50,
+      men: 25,
+      women: 25 
+    },{ 
+      three_star: 100,
+      men: 90,
+      women: 10 
+    }]
+  }
+]
+module.exports = products
---
+++ part1/server.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var graphql = require('graphql');
+var GraphQLSchema = graphql.GraphQLSchema;
+var GraphQLObjectType = graphql.GraphQLObjectType;
+var GraphQLString = graphql.GraphQLString;
+var GraphQLList = graphql.GraphQLList;
+var GraphQLInt = graphql.GraphQLInt;
+var products = require('./data');
+var app = express();
+var PORT = process.env.port || 3000
+var productType = new GraphQLObjectType({
+  name: "products",
+  description: "Detail of The product",
+  fields: () => ({
+   name: {
+     type: GraphQLString,
+     description: "Name of the product",
+   },
+   price: {
+     type: GraphQLInt,
+     description: "price of product",
+   },
+   category: {
+     type: new GraphQLList(GraphQLString),
+     description: "category of product",
+   }
+ })
+});
+var queryType = new GraphQLObjectType({
+  name: "queryProduct",
+  description: "query of product",
+  fields: () => ({
+    hey: {
+      type: GraphQLString,
+      resolve: function(_, args){
+        return products[0].name
+      }
+    },
+    getProducts: {
+      type: new GraphQLList(productType),
+      resolve: function(_, args){
+        return products
+      }
+    }
+  })
+});
+var MyGraphQLSchema = new GraphQLSchema({
+  query: queryType
+});
+app.use('/graphql', graphqlHTTP({
+  schema: MyGraphQLSchema,
+  graphiql: true
+}));
+app.listen(PORT);
+console.log("Server running on localhost:", PORT);
